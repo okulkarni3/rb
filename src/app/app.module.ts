5ab455a5771b8778b8a9bea3013809ccf00b17ae
okulkarni@acumensolutions.com <okulkarni@acumensolutions.com>
2018-06-01 16:26:55 -0400

    Committing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,26 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { MatSnackBarModule, MatStepperModule,  MatIconModule, MatInputModule, MatDividerModule, MatCardModule, MatButtonModule, MatGridListModule } from '@angular/material';
+import { MatSnackBarModule, MatStepperModule, MatToolbarModule, MatIconModule, MatInputModule, MatDividerModule, MatCardModule, MatButtonModule, MatGridListModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './/app-routing.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     MatSnackBarModule,
+    MatToolbarModule,
     MatInputModule,
     ReactiveFormsModule,
     MatStepperModule,
---
+++ src/app/main-toolbar/main-toolbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-main-toolbar',
+  templateUrl: './main-toolbar.component.html',
+  styleUrls: ['./main-toolbar.component.css']
+})
+export class MainToolbarComponent implements OnInit {
+
+  constructor( private router: Router) { }
+  ngOnInit() {
+  }
+
+}
